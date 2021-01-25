d1a58822c3a53c09d71cf306934c7c12a719ab88
James Arthur <thruflo@gmail.com>
2021-01-25 14:24:09 +0100

    nix: remove unused deps from `shell.nix`.

--- shell.nix
+++ shell.nix
@@ -6,14 +6,13 @@ let
   inherit (lib) optional optionals;
 
   elixir = beam.packages.erlangR23.elixir_1_11;
-  node = nodejs-12_x;
   postgres = postgresql_12;
 
   phoenixVersion = "1.5.7";
 in
 
 mkShell {
-  buildInputs = [ elixir git glibcLocales kind kubectl node postgres ]
+  buildInputs = [ elixir git glibcLocales postgres ]
     ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
     ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
       # For file_system on macOS.
@@ -37,16 +36,8 @@ mkShell {
     then
       mkdir -p $MIX_ARCHIVES
 
-      # Locales are flakey, so hack utf-8 here.
       LC_CTYPE=C.UTF-8 mix local.hex --force
       LC_CTYPE=C.UTF-8 mix archive.install --force hex phx_new ${phoenixVersion}
-    fi
-
-    export KUBEDIR="$PWD/.kube"
-    export KUBECONFIG="$KUBEDIR/config"
-    if [ ! -d "$KUBEDIR" ]
-    then
-      mkdir -p $KUBEDIR
     fi
   '';
 }
